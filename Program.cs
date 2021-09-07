43f3d8ac2c944353a687a755898847e47bfeb256
Darshan Magare <darshan.magare1234@gmail.com>
2021-09-07 08:28:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace CalculatorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 sq = new Class1();
+
+            Console.WriteLine("Square of the entered number is {0}", sq.Square(12));
+            Console.WriteLine(sq.Add(8, 63));
+            Console.WriteLine(sq.Multiply(5, 8));
+            Console.WriteLine(sq.Subtract(10, 42));
+            Console.WriteLine(sq.Division(3, 4));
+            Console.ReadLine();
+        }
+    }
+}
